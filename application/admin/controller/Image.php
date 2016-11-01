75f43e16b4e6869e4242ae758e998d000da435b9
diebug <979982390@qq.com>
2016-11-01 20:17:25 +0800

    xbenshan

--- application/admin/controller/Image.php
+++ application/admin/controller/Image.php
@@ -19,30 +19,27 @@ class Image extends Controller
     }
     
     public function upload()
-    {
+    {   
         $model= model('Image');
-        if(Request::instance()->isPost())
-        {
-            $data=Request::instance()->post();
-             // 获取表单上传文件 例如上传了001.jpg
+        if(Request::instance()->isPost()) //如果是POST提交
+        {    // 获取经过过滤的全部post变量
+            $data=Request::instance()->post();  
+             // 获取表单上传文件 
             $file = request()->file('image');
             // 移动到框架应用根目录/public/uploads/ 目录下
             $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
             if($info){
-                // 成功上传后 获取上传信息
-                // 输出 jpg
-               // echo $info->getExtension();
-                // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
-               $data['image']= $info->getFilename();
-                // 输出 42a79759f284b767dfcb2a0197904287.jpg
-                //echo $info->getFilename(); 
+                //获得文件名
+               $data['image']= $info->getFilename();   
+               //缩略图
+                
             }else{
                 // 上传失败获取错误信息
                 echo $file->getError();
             }
         }
-        
-        $model->allowField(true)->save($data);
+        // 过滤post数组中的非数据表字段数据
+        $model->allowField(true)->save($data); 
     }
     
     
