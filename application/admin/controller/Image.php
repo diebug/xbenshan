a1ae99cf98d099dd7cd8c9f97eb8b8cfe05219ce
diebug <979982390@qq.com>
2016-11-11 21:13:20 +0800

    xbenshan

--- application/admin/controller/Image.php
+++ application/admin/controller/Image.php
@@ -12,33 +12,48 @@ class Image extends Controller
     //显示列表页面
     public function imageList()
     {
-        $model=model('Image');
+        $model = model('Image');
         //取出所有数据
-        $data=$model->select();
+        $data = $model->select();
         //取出总记录数
-        $count=$model->Count();
+        $count = $model->Count();
         $this->assign([
-            'data'  => $data,
+            'data' => $data,
             'count' => $count
         ]);
         return $this->fetch();
-    }    
-      //添加信息 
+    }
+    //是否发布
+    public function isOnSale(){
+        $image_id = input('image_id');
+        if($image_id){
+          //获得isOnSale的值 0是下架 1是发布
+          $info = Db::table('x_image')->where('id',$image_id)->value('is_on_sale');
+          if($info==1){
+            Db::table('x_image')->where('id',$image_id)->setField('is_on_sale', '0');
+            echo 0;
+          }else{
+              Db::table('x_image')->where('id',$image_id)->setField('is_on_sale', '1');
+                echo 1;
+          }
+        }
+    }
+      //添加信息
     public function imageAdd()
-    {	  
+    {
             //如果是POST提交
-        if(Request::instance()->isPost()) 
-        {    
+        if(Request::instance()->isPost())
+        {
             // 获取经过过滤的全部post变量
-            $data=Request::instance()->post(); 
-                    // 获取表单上传文件 
+            $data=Request::instance()->post();
+                    // 获取表单上传文件
             $file = request()->file('image');
             // 移动到框架应用根目录/public/uploads/ 目录下
             $path=ROOT_PATH . 'public' . DS . 'uploads/';
             $info = $file->move($path);
             if($info){
                 //获得文件名
-               $data['image']= $info->getFilename();  
+               $data['image']= $info->getFilename();
 
                //缩略图
 
@@ -57,16 +72,16 @@ class Image extends Controller
             $data['create_time']=time();
             $model= model('Image');
             if($model->allowField(true)->save($data))
-                $this->success('添加成功', 'Image/imageList');
+                $this->success('添加成功', 'Image/imageList','',1);
             else
-                $this->error('添加失败', 'Image/imageList');
-            exit;    
+                $this->error('添加失败', 'Image/imageList','',1);
+            exit;
         }
         return $this->fetch();
     }
         //图片信息编辑
      public function imageEdit()
-    {	
+    {
         $model = model('Image');
          //获取图片信息ID
         $image_id = input('image_id');
@@ -75,30 +90,31 @@ class Image extends Controller
             //获取对应ID 信息
            $info = $model->where('id', $image_id) ->find();
         }
-        if(Request::instance()->isPost()) 
-        {   
+        if(Request::instance()->isPost())
+        {
             // 获取经过过滤的全部post变量
-            $data=Request::instance()->post(); 
+            $data=Request::instance()->post();
             $data['update_time']=time();
             if($model->isUpdate(true)->allowField(true)->save($data,['id' =>$data['image_id']]))
-                $this->success('修改成功', 'Image/imageList');
+                $this->success('修改成功', 'Image/imageList','',1);
             else
-                $this->error('修改失败', 'Image/imageList');
+                $this->error('修改失败', 'Image/imageList','',1);
             exit;
-        } 
+        }
         $this->assign('data',$info);
         return $this->fetch();
     }
-    
+
     //图片信息删除
     public function  imageDel()
     {
         $image_id = input('image_id');
-       
         if(Db::table('x_image')->delete($image_id))
-             $this->success('删除成功', 'Image/imageList');
+            // $this->success('删除成功', 'Image/imageList');
+            return true;
         else
-             $this->error('删除失败', 'Image/imageList');
+             return false;
+        exit;
     }
-    
+
 }
